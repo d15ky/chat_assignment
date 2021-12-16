40b04f838e0fbd2ef8dc41840894980c792c6723
Oleksii Martynovskyi <o.martynovskyi@gmail.com>
2021-12-17 00:47:36 +0200

    code formatted

--- lib/chat_assignment_web/controllers/user_session_controller.ex
+++ lib/chat_assignment_web/controllers/user_session_controller.ex
@@ -2,24 +2,34 @@ defmodule ChatAssignmentWeb.UserSessionController do
   use ChatAssignmentWeb, :controller
 
   import Phoenix.HTML.Link, only: [link: 2]
+  import ChatAssignment.Accounts, only: [get_user_by_login_and_password: 2]
 
-  alias ChatAssignment.Accounts
   alias ChatAssignmentWeb.UserAuth
 
   def new(conn, _params) do
-    render(conn, "new.html", error_message: nil)
+    render(conn, "new.html")
   end
 
   def create(conn, %{"user" => user_params}) do
     %{"login" => login, "password" => password} = user_params
 
-    case Accounts.get_user_by_login_and_password(login, password) do
-      nil ->render_with_error(conn, "Invalid login (username or email) or password")
-      user when user.confirmed_at != nil -> UserAuth.log_in_user(conn, user, user_params)
-      user when user.confirmed_at == nil -> render_with_error(conn,
-      ["You need to ", link("confirm your email", to: Routes.user_confirmation_path(conn, :new)), " before you'll be able to login."])
-    end
+    case get_user_by_login_and_password(login, password) do
+      nil ->
+        render_with_error(conn, "Invalid login (username or email) or password")
 
+      user when user.confirmed_at != nil ->
+        UserAuth.log_in_user(conn, user, user_params)
+
+      user when user.confirmed_at == nil ->
+        render_with_error(
+          conn,
+          [
+            "You need to ",
+            link("confirm your email", to: Routes.user_confirmation_path(conn, :new)),
+            " before you'll be able to login."
+          ]
+        )
+    end
   end
 
   defp render_with_error(conn, message) do
