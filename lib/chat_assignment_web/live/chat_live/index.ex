6bccf29c0ecd3cb20665e85685cc1faf19516d93
Oleksii Martynovskyi <o.martynovskyi@gmail.com>
2021-12-16 18:36:15 +0200

    pubsub system added and new messages handled

--- lib/chat_assignment_web/live/chat_live/index.ex
+++ lib/chat_assignment_web/live/chat_live/index.ex
@@ -1,33 +1,65 @@
 defmodule ChatAssignmentWeb.ChatLive.Index do
   use ChatAssignmentWeb, :live_view
 
+  @pubsub_topic "chat_main"
+
   import ChatAssignment.Accounts, only: [get_user_by_session_token: 1]
+
+  alias ChatAssignmentWeb.Endpoint
+  alias ChatAssignmentWeb.ChatPresence
 
   alias ChatAssignment.Chat
   alias ChatAssignment.Chat.Message
 
   @impl true
   def mount(_params, session, socket) do
+    # Initial values for assigns
     message_changeset = Chat.change_message(%Message{})
     user = get_user_by_session_token(session["user_token"])
-    last_messages = Chat.last_messages
+    last_messages = Chat.last_messages()
+
+    # Start PubSub and Presence system for tracking messages and users
+    Endpoint.subscribe(@pubsub_topic)
+
+    {:ok, _} =
+      ChatPresence.track(self(), @pubsub_topic, session["user_token"], %{
+        user: user,
+        online_at: inspect(System.system_time(:second))
+      })
+
     {:ok,
-    socket
-    |> assign(messages: last_messages)
-    |> assign(message_changeset: message_changeset)
-    |> assign(user: user)
-  }
+     socket
+     |> assign(messages: last_messages)
+     |> assign(message_changeset: message_changeset)
+     |> assign(user: user)}
   end
 
   @impl true
   def handle_event("new_message", %{"message" => message}, socket) do
-
-    {:ok, _message_struct} =
+    {:ok, message_data} =
       message
       |> Map.put("user_id", socket.assigns.user.id)
-      |> Chat.create_message
+      |> Chat.create_message()
+    message_data = %{message_data | user: socket.assigns.user}
+
+    Endpoint.broadcast(@pubsub_topic, "new_message", message_data)
 
     {:noreply, socket}
   end
 
+  @impl true
+  def handle_info(%{event: "new_message", payload: message}, socket) do
+    messages = [message | socket.assigns.messages]
+
+    {:noreply,
+     socket
+     |> assign(messages: messages)}
+  end
+
+  @impl true
+  def handle_info(%{event: "presence_diff", payload: _}, socket) do
+
+    {:noreply,
+     socket}
+  end
 end
