d2040a13a30f053feacfc592efe3648f6d3ec3b2
Oleksii Martynovskyi <o.martynovskyi@gmail.com>
2021-12-16 13:55:28 +0200

    prevent user from logging in before email is confirmed

--- lib/chat_assignment_web/controllers/user_session_controller.ex
+++ lib/chat_assignment_web/controllers/user_session_controller.ex
@@ -11,12 +11,12 @@ defmodule ChatAssignmentWeb.UserSessionController do
   def create(conn, %{"user" => user_params}) do
     %{"email" => email, "password" => password} = user_params
 
-    if user = Accounts.get_user_by_email_and_password(email, password) do
-      UserAuth.log_in_user(conn, user, user_params)
-    else
-      # In order to prevent user enumeration attacks, don't disclose whether the email is registered.
-      render(conn, "new.html", error_message: "Invalid email or password")
+    case Accounts.get_user_by_email_and_password(email, password) do
+      nil -> render(conn, "new.html", error_message: "Invalid login (username or email) or password")
+      user when user.confirmed_at != nil -> UserAuth.log_in_user(conn, user, user_params)
+      user when user.confirmed_at == nil -> render(conn, "new.html", error_message: "You need to confirm your email before you'll be able to login.")
     end
+
   end
 
   def delete(conn, _params) do
